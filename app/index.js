f18343018ae1d2aed278fc54237e6d97808fb33c
Pearce <pearce.ropion@me.com>
2019-03-03 11:55:30 -0800

    Added material UI

--- app/index.js
+++ app/index.js
@@ -4,13 +4,12 @@ import { render } from 'react-dom';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { AxiosProvider } from 'react-axios';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import SynergyStore from './Store';
-import { Heading } from './components/Heading';
-import ApplicationContainer from './components/ApplicationContainer';
-import UserHandlerContainer from './components/login/UserHandlerContainer';
-
-import 'semantic-ui-css/semantic.min.css';
+// import { Heading } from './components/Heading';
+// import ApplicationContainer from './components/ApplicationContainer';
+// import UserHandlerContainer from './components/login/UserHandlerContainer';
 
 const store = SynergyStore.store;
 const history = SynergyStore.history;
@@ -25,9 +24,10 @@ render(
         <Provider store={store}>
             <Router history={history}>
                 <React.Fragment>
+                    <CssBaseline />
                     {/* <UserHandlerContainer /> */}
-                    <Heading />
-                    <ApplicationContainer history={history} />
+                    {/* <Heading /> */}
+                    {/* <ApplicationContainer history={history} /> */}
                 </React.Fragment>
             </Router>
         </Provider>
