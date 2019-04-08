834357a6776cdfaff8fcf6485c9cfdf54070e039
Pearce Ropion <propion@proofpoint.com>
2019-04-07 19:09:58 -0700

    Fixed sorting in add menu

--- app/api/sort.js
+++ app/api/sort.js
@@ -5,3 +5,33 @@ export const sortByStringProperty = property => (entityA, entityB) =>
 
 export const sortByNumericProperty = property => (entityA, entityB) =>
     get(entityA, property, 0) > get(entityB, property, 0) ? 1 : -1;
+
+export const sortMap = {
+    channelID: entity => get(entity, 'channelID', ''),
+    deviceID: entity => get(entity, 'deviceID', ''),
+    groupID: entity => get(entity, 'groupID', ''),
+    userID: entity => get(entity, 'userID', ''),
+    chartID: entity => get(entity, 'chartID', ''),
+    uuid: entity => get(entity, 'uuid', ''),
+
+    name: entity => get(entity, 'name', ''),
+    type: entity => get(entity, 'type', ''),
+    members: entity => get(entity, 'members', 0),
+    channels: entity => get(entity, 'channels', 0),
+
+    amps: entity => get(entity, 'amps', 0),
+    time: entity => get(entity, 'time', new Date()),
+    chartType: entity => get(entity, 'chartType', ''),
+    usageType: entity => get(entity, 'usageType', ''),
+
+    email: entity => get(entity, 'email', ''),
+    family_size: entity => get(entity, 'family_size', 0),
+
+    created: entity => get(entity, 'created', new Date()),
+    updated: entity => get(entity, 'updated', new Date()),
+    added: entity => get(entity, 'added', new Date()),
+    from: entity => get(entity, 'from', new Date()),
+    until: entity => get(entity, 'until', new Date()),
+};
+
+export const getSortFunctions = fields => fields.map(sortee => sortMap[sortee]);
